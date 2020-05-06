5a1796be15109e88aa57005264bedcc762af4f18
mzhang181 <mzhang181@gmail.com>
2020-05-05 22:00:19 -0400

    Created placeholder route functions

--- api/api.py
+++ api/api.py
@@ -1,9 +1,19 @@
+import time
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello_world():
    return 'Hello World'
+
+@app.route('/<date>')
+def date(date):
+    return str(date)
+
+@app.route('/time')
+def get_time():
+    return {'time': time.time()}
+    
 
 if __name__ == '__main__':
       app.run()
