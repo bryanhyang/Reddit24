b41c353f3be90225d039f725936c859013b79dd8
mzhang181 <mzhang181@gmail.com>
2020-05-05 23:24:36 -0400

    Date comparison added

--- api/api.py
+++ api/api.py
@@ -1,4 +1,5 @@
-import time
+from datetime import datetime
+import sys
 from flask import Flask
 app = Flask(__name__)
 
@@ -8,7 +9,21 @@ def hello_world():
 
 @app.route('/<date>')
 def date(date):
-    return str(date)
+    try:
+        target = datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        print('Invalid date', file=sys.stderr)
+        return {'date': ''}
+    current = datetime.today()
+    diff = current - target
+    if diff.days > 0:
+        print('Valid date', file=sys.stderr)
+        return {'date': str(date)}
+    else:
+        print('Invalid date', file=sys.stderr)
+        return {'date': ''}
+
+    return {'date': str(date)}
 
 @app.route('/time')
 def get_time():
