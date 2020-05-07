f3dc901858152ba33ce018fe7276083be04d8097
mzhang181 <mzhang181@gmail.com>
2020-05-06 21:06:56 -0400

    Added background scheduling for DB

--- api/api.py
+++ api/api.py
@@ -1,8 +1,16 @@
-from datetime import datetime
+import atexit
 import sys
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
 from flask import Flask
 app = Flask(__name__)
 
+# Global Variables ----------------------------------------
+today = datetime.today()
+
+
+# Routing -------------------------------------------------
 @app.route('/hello')
 def hello_world():
    return 'Hello World'
@@ -14,8 +22,7 @@ def date(date):
     except ValueError:
         print('Invalid date', file=sys.stderr)
         return {'date': ''}
-    current = datetime.today()
-    diff = current - target
+    diff = today - target
     if diff.days > 0:
         print('Valid date', file=sys.stderr)
         return {'date': str(date)}
@@ -25,10 +32,27 @@ def date(date):
 
     return {'date': str(date)}
 
+@app.route('/')
+def landing():
+   return 'Hello World'
+
 @app.route('/time')
 def get_time():
     return {'time': time.time()}
-    
+
+# Non-routing functions -----------------------------------
+def updateDB():
+    print('EEE ERR', file=sys.stderr)
+
+
+# Scheduler -----------------------------------------------
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=updateDB, trigger="interval", seconds=3) # set to 3 seconds for testing, otherwise 3600
+scheduler.start()
+
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
 
 if __name__ == '__main__':
-      app.run()
+      app.run(use_reloader=False)
