89e2e4a0607d1ff37110867290e5f00a356d335c
mzhang181 <mzhang181@gmail.com>
2020-05-07 19:50:36 -0400

    MongoDB model created

--- api/api.py
+++ api/api.py
@@ -1,24 +1,43 @@
 import atexit
 import prawPull
 import sys
+import json
 
 from os import environ
 from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_mongoengine import MongoEngine
 app = Flask(__name__)
 
 # Global Variables ----------------------------------------
 today = datetime.today()
 
+# MongoDB -------------------------------------------------
+
 MONGODB_URI = environ.get('MONGODB')
+
+app.config["MONGODB_HOST"] = DB_URI
+
+db = MongoEngine(app)
+db.init_app(app)
+
+# creating model
+class Submission(db.EmbeddedDocument):
+    image = db.URLField(required=True))
+    link = db.URLField(required=True))
+
+class Day(db.Document):
+    date = db.DateTimeField(required=True)
+    submissions = db.ListField(EmbeddedDocumentField(Submission))
+
 
 # Routing -------------------------------------------------
 @app.route('/hello')
 def hello_world():
    return 'Hello World'
 
-@app.route('/<date>')
+@app.route('/date/<date>')
 def date(date):
     try:
         target = datetime.strptime(date, "%Y-%m-%d")
