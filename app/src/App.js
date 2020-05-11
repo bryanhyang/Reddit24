a8efd02780d4bed2f7f8f4ed408c02ae9c405950
mzhang181 <mzhang181@gmail.com>
2020-05-10 22:18:13 -0400

    Used react-bootstrap instead

--- app/src/App.js
+++ app/src/App.js
@@ -1,56 +1,59 @@
 import React from 'react';
 import './App.css';
-//import ImageFrame from './components/ImageFrame'
 import Gallery from 'react-photo-gallery';
 import Calendar from 'react-calendar';
+// import 'react-calendar/dist/Calendar.css';
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Navbar from 'react-bootstrap/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-calendar/dist/Calendar.css';
-import Sidebar from "react-sidebar"
 
-const mql = window.matchMedia(`(min-width: 720px)`);
 
 class App extends React.Component {
 	state = {
-    	recvData : false,
-		submissions : {},
-		images : {},
-		date : new Date()
+		recvData: false,
+		submissions: {},
+		images: {},
+		date: new Date()
 	};
-	 // this is a comment 
+	// this is a comment 
 
 	componentDidMount = () => {
 		this.getReddit();
 	};
 
-	getReddit = (suffix='/today') => {
+	getReddit = (suffix = '/today') => {
 		//may be a more efficient way to reset recvData
-		this.setState ({
-			recvData : false
+		this.setState({
+			recvData: false
 		});
 		fetch(suffix)
 			.then(res => res.json())
-		  	.then(json => {	
+			.then(json => {
 				let newSubmissions = {};
 				let newImages = [];
 				json['submissions'].forEach(submission => {
-						let tmp = {}
-						tmp['src'] = submission.image
-						tmp['width'] = 4
-						tmp['height'] = 3
-						newImages.push(tmp)
-						newSubmissions[submission.image] = submission.link
-						
+					let tmp = {}
+					tmp['src'] = submission.image
+					tmp['width'] = 4
+					tmp['height'] = 3
+					newImages.push(tmp)
+					newSubmissions[submission.image] = submission.link
+
 				});
-				
-		  		this.setState({
+
+				this.setState({
 					recvData: true,
 					submissions: newSubmissions,
-					images : newImages
+					images: newImages
 				})
 				console.log(newImages)
-				  
+
 			})
-		  	.catch((error) => {
-		   	 	console.error('Error:', error);
+			.catch((error) => {
+				console.error('Error:', error);
 			});
 	}
 
@@ -61,69 +64,42 @@ class App extends React.Component {
 	}
 
 	newDate = (date) => {
-		let past = '/date/' + date.toJSON().slice(0,10);
+		let past = '/date/' + date.toJSON().slice(0, 10);
 		console.log(past)
 		this.getReddit(past);
 	}
-	constructor(props) {
-		super(props);
-		this.state = {
-			sidebarDocked: mql.matches,
-			sidebarOpen: false
-		};
 
-		this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
-		this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
-	}
-	componentWillMount() {
-        mql.addListener(this.mediaQueryChanged);
-    }
-
-    componentWillUnmount() {
-        this.state.mql.removeListener(this.mediaQueryChanged);
-    }
-
-    onSetSidebarOpen(open) {
-        this.setState({ sidebarOpen: open });
-    }
-
-    mediaQueryChanged() {
-        this.setState({ sidebarDocked: mql.matches, sidebarOpen: false });
-    }
-
-	render(){
+	render() {
 		let { recvData, submissions } = this.state;
 
 
-		if(!recvData){
-			return(
+		if (!recvData) {
+			return (
 				<div className="App">
 					Loading . . .
 				</div>
 			);
 
 		}
-		else{
-    		return (  
-    	  		<div className="App">
-      				helloworld
+		else {
+			return (
+				<div className="App">
 					{console.log(recvData)};
-					<Sidebar
-						sidebar={
-							<Calendar
-								onChange={this.newDate}
-								value={this.state.date}
-							/>}
-						open={this.state.sidebarOpen}
-						docked={this.state.sidebarDocked}
-						onSetOpen={this.onSetSidebarOpen}
-						styles={{ sidebar: { background: "white" } }}
-					>
-					<Gallery photos={this.state.images} onClick = {this.openLink}/>
-					</Sidebar>
+					<Container fluid>
+						<Navbar variant="white" bg="dark"> Hello
+						</Navbar>
+						<Row>
+							<Col md="auto">
+								<Calendar> onChange={this.newDate} value={this.state.date} </Calendar>
+							</Col>
+							<Col>
+								<Gallery photos={this.state.images} onClick={this.openLink} />
+							</Col>
+						</Row>
+					</Container>
 				</div>
-    		);
-  		}
+			);
+		}
 	}
 }
 export default App;
